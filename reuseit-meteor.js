41ae60f493a2cbed690f66093afbda489665859a
luchux <lucianoguasco@gmail.com>
2013-07-10 22:28:39 +1000

    Added Data Store MongoDB for UnusedObjects

--- reuseit-meteor.js
+++ reuseit-meteor.js
@@ -1,6 +1,10 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to reuseit-meteor.";
+
+  //Creation of DataStore
+  UnusedObjects = new Meteor.Collection('uobjects')
+
+  Template.unused_objects = function () {
+    return UnusedObjects.find({}).fetch();
   };
 
   Template.hello.events({
@@ -13,6 +17,19 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  //Creation of DataStore
+  UnusedObjects = new Meteor.Collection('uobjects')
+  //Popoulate the db
+  UnusedObjects.insert({
+    "date_cyrcles ": "2013-06-16",
+    "date_public": "2013-06-16",
+    "descr": "testing",
+    "id": 7,
+    "image": "http://localhost:8000/media/images/None/SAM_2344.JPG",
+    "name": "test",
+    "resource_uri": "/api/object/7/"
+  });
+
   Meteor.startup(function () {
     // code to run on server at startup
   });
