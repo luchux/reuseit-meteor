8a3d7e729100cf99a4d028f5bb2c9c6afc6c5790
luchux <lucianoguasco@gmail.com>
2013-07-12 00:44:26 +1000

    scripts splitting server client

--- server/server.js
+++ server/server.js
@@ -7,40 +7,60 @@ if (Meteor.isServer) {
   UnusedObjects.insert({
     "date_cyrcles ": "2013-06-16",
     "date_public": "2013-06-16",
-    "descr": "Camel in excellent conditions",
+    "descr": "TV Plasma 27\" Philips, works perfect, remote control" ,
     "id": 1,
-    "image": "objects_images/2.JPG",
-    "name": "Camel",
+    "image": "objects_images/1.jpg",
+    "name": "TV Plasma",
     "resource_uri": "/api/object/7/"
   });
 
   UnusedObjects.insert({
     "date_cyrcles ": "2013-06-16",
     "date_public": "2013-06-16",
-    "descr": "Ukelele Thenesis like new, good strings",
+    "descr": "Bycicle. Frame from 80s, new wheels, works well",
     "id": 2,
-    "image": "objects_images/1.JPG",
-    "name": "Ukelele",
+    "image": "objects_images/2.jpg",
+    "name": "80s Bike",
     "resource_uri": "/api/object/7/"
   });
 
   UnusedObjects.insert({
     "date_cyrcles ": "2013-06-16",
     "date_public": "2013-06-16",
-    "descr": "Turtle in excellent conditions",
+    "descr": "Good weights for training hard, 40ks set. ",
     "id": 3,
-    "image": "objects_images/3.JPG",
-    "name": "Tortuga",
+    "image": "objects_images/3.jpg",
+    "name": "Weights!",
     "resource_uri": "/api/object/7/"
   });
 
   UnusedObjects.insert({
     "date_cyrcles ": "2013-06-16",
     "date_public": "2013-06-16",
-    "descr": "Morris 79 with new engine",
+    "descr": "Table ideal for living, wood, excellent conditions.",
     "id": 4,
-    "image": "objects_images/4.JPG",
-    "name": "Morris",
+    "image": "objects_images/4.jpg",
+    "name": "Small Table",
+    "resource_uri": "/api/object/7/"
+  });
+
+  UnusedObjects.insert({
+    "date_cyrcles ": "2013-06-16",
+    "date_public": "2013-06-16",
+    "descr": "Backpack Montagne, 60 litters. Used for 4 months. Comfortable.",
+    "id": 5,
+    "image": "objects_images/5.jpg",
+    "name": "Backpack 60L",
+    "resource_uri": "/api/object/7/"
+  });
+
+  UnusedObjects.insert({
+    "date_cyrcles ": "2013-06-16",
+    "date_public": "2013-06-16",
+    "descr": "Climbing shoes La Sportiva, Cobra. size 40, good grip for edges.",
+    "id": 6,
+    "image": "objects_images/6.jpg",
+    "name": "Climbing Shoes 40",
     "resource_uri": "/api/object/7/"
   });
 
