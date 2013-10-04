76c0b23364a974f8b4962afd0f5cb990c3575b11
luchux <lucianoguasco@gmail.com>
2013-10-04 13:30:09 +1000

    new templates and ux

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 
 //Publish those unusedObjects that a user can request.
 Meteor.publish('objects-others', function () {
-  return UnusedObjects.find({ 'user': { $ne: this.userId }, 'requests': { $nin : [this.userId] }
+  return UnusedObjects.find({ 'user': { $ne: this.userId }, 'requests': { $nin :[this.userId] }, 'forgets': { $nin: [this.userId] }
    });
+});
+
+
+//Publish those unusedObjects that a user can request.
+Meteor.publish('objects-requested', function () {
+  return UnusedObjects.find({ 'user': { $e: this.userId }, 'requests.1': {$exists: 1}
+  });
 });
 
 Meteor.startup(function () {
